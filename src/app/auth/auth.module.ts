73bc6ef7c4fe79f1800beb2639ed4a24bef3604a
snowyyu <731444920@qq.com>
2020-11-12 23:51:41 +0800

    编辑登录页面

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
@@ -21,6 +22,7 @@ import { RouterModule } from '@angular/router';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatToolbarModule,
     ReactiveFormsModule,
     RouterModule.forChild([{ path: 'login', component: LoginComponent }]),
   ],
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
-interface User {
-  username?: string;
-  password?: string;
-}
+// export class MaterialMatcher implements ErrorStateMatcher {
+//   isErrorState(
+//     control: FormControl | null,
+//     form: FormGroupDirective | NgForm | null
+//   ): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(
+//       control &&
+//       control.invalid &&
+//       (control.dirty || control.touched || isSubmitted)
+//     );
+//   }
+// }
+
+// export class MaterialMatcherPassword implements ErrorStateMatcher {
+//   isErrorState(
+//     control: FormControl | null,
+//     form: FormGroupDirective | NgForm | null
+//   ): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(
+//       control &&
+//       control.invalid &&
+//       (control.dirty || control.touched || isSubmitted)
+//     );
+//   }
+// }
 
 @Component({
   selector: 'app-login',
@@ -11,10 +41,18 @@ interface User {
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  user: User = {};
-  errorMsg: string = null;
   constructor() {}
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
 
-  ngOnInit(): void {}
-  login(): void {}
+  // passwordFormControl = new FormControl('', [Validators.required]);
+
+  // version = VERSION.full;
+
+  // errorMatcher = new MaterialMatcher();
+  // errorMatcherPassword = new MaterialMatcherPassword();
+
+  ngOnInit() {}
 }
